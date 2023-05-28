88fbf7732b8ae91679d8771239fb16f679851bf9
leosundstrom <112473538+leosundstrom@users.noreply.github.com>
2023-05-28 23:23:53 +0200

    exploringfinished

---
+++ finalProject/decisions.cs
@@ -0,0 +1,16 @@
+public class Decisions
+
+{
+    classManager cM;
+   
+
+
+    public Decisions(classManager cM)
+    {
+        this.cM = cM;
+      
+    }
+     public void Update(){
+   
+}
+}
---
+++ finalProject/exploring.cs
@@ -0,0 +1,140 @@
+using System;
+
+public class Exploring
+{
+    classManager cM;
+    bool[,] map = new bool[13, 9];
+    int playerX = 1;
+    int playerY = 1;
+    Random generator = new();
+
+    public Exploring(classManager cM)
+    {
+        this.cM = cM;
+
+        //y and x cords of the player
+        playerX = 6;
+        playerY = 6;
+
+        //Places the rooms
+     map[6, 6] = true; // Bottom row path (center)
+    map[6, 5] = true; // Start
+
+    map[5, 4] = true;
+    map[7, 4] = true;
+    map[3, 4] = true;
+    map[9, 4] = true;
+
+    map[5, 3] = true;
+    map[7, 3] = true;
+    map[3, 3] = true;
+    map[9, 3] = true;
+
+    map[5, 2] = true;
+    map[7, 2] = true;
+    map[5, 5] = true;
+    map[7, 5] = true;
+    map[3, 2] = true;
+    map[9, 2] = true;
+    map[4, 4] = true;
+    map[8, 4] = true;
+
+
+    }
+
+
+
+    // Draws the map (Borrowed code, don't fully understand how it works but essential for my game)
+    public void DrawMap()
+    {
+        
+        for (var y = 0; y < map.GetLength(1); y++)
+        {
+            for (var x = 0; x < map.GetLength(0); x++)
+            {
+                if (map[x, y] == true)
+                {
+                    if (playerX == x && playerY == y)
+                    {
+                        Console.Write("1");
+                    }
+
+                    else
+                    {
+                        Console.Write("0");
+                    }
+                }
+                else
+                {
+                    Console.Write("#");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+
+
+
+    // Draws the map and allows the player to move through it
+    public void Update()
+{
+    Console.Clear();
+    DrawMap();
+    string text;
+    bool move = true;
+
+    while (move)
+    {
+        text = Console.ReadLine();
+
+        if (text.ToLower() == "up")
+        {
+            if (playerY > 0 && map[playerX, playerY - 1])
+            {
+                playerY--;
+                move = false;
+            }
+            else
+            {
+                Console.WriteLine("You can't go that way");
+            }
+        }
+        else if (text.ToLower() == "down")
+        {
+            Console.WriteLine("There's no turning back");
+        }
+        else if (text.ToLower() == "left")
+        {
+            if (playerX > 0 && map[playerX - 1, playerY])
+            {
+                playerX--;
+                move = false;
+            }
+            else
+            {
+                Console.WriteLine("You can't go that way");
+            }
+        }
+        else if (text.ToLower() == "right")
+        {
+            if (playerX < map.GetLength(0) - 1 && map[playerX + 1, playerY])
+            {
+                playerX++;
+                move = false;
+            }
+            else
+            {
+                Console.WriteLine("You can't go that way");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Try again");
+        }
+    }
+
+    
+}
+
+
+}
